daf7fae13ed8160edcdfb7c367baf3aa87ce8384
T1 <126980610+tadash10@users.noreply.github.com>
2023-04-04 23:43:09 -0300

    Create v4.sh

---
+++ v4.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+# Define authorized users and their roles
+declare -A authorized_users=(
+    ["admin"]="admin_password"
+    ["user"]="user_password"
+)
+
+# Define user roles and access levels
+declare -A user_roles=(
+    ["admin"]="admin"
+    ["user"]="user"
+)
+
+# Define session timeout (in seconds)
+SESSION_TIMEOUT=300
+
+# Function to prompt user for credentials and validate against authorized users
+function authenticate_user {
+    read -p "Username: " username
+    read -s -p "Password: " password
+    echo
+
+    if [[ ${authorized_users[$username]} == $password ]]; then
+        echo "Welcome, $username!"
+        USER_ROLE=${user_roles[$username]}
+    else
+        echo "Invalid credentials. Please try again."
+        exit 1
+    fi
+}
+
+# Function to implement two-factor authentication (2FA) using Google Authenticator
+function authenticate_2fa {
+    read -p "Enter verification code: " verification_code
+    VALID_CODE=$(google-authenticator -d -r 30 -f - -w 3 -q | grep -o -E '[0-9]{6}')
+    if [[ $verification_code == $VALID_CODE ]]; then
+        echo "Authentication successful!"
+    else
+        echo "Invalid verification code. Please try again."
+        exit 1
+    fi
+}
+
+# Function to handle user logout
+function logout_user {
+    echo "Logging out..."
+    unset USER_ROLE
+}
+
+# Function to check if session has timed out
+function check_session_timeout {
+    if [[ $(($(date +%s) - ${SESSION_START_TIME})) -ge ${SESSION_TIMEOUT} ]]; then
+        echo "Session timed out. Please log in again."
+        logout_user
+    fi
+}
+
+# Function to log all authentication attempts
+function log_authentication_attempt {
+    timestamp=$(date +"%Y-%m-%d %T")
+    echo "${timestamp} - ${username} - ${USER_ROLE}" >> authentication.log
+}
+
+# Main function to handle user sessions
+function main {
+    echo "Welcome to the secure system!"
+    authenticate_user
+    log_authentication_attempt
+
+    # Check user role and access level to determine 2FA requirement
+    if [[ $USER_ROLE == "admin" ]]; then
+        echo "Admin access level detected. 2FA required."
+        authenticate_2fa
+    elif [[ $USER_ROLE == "user" ]]; then
+        echo "User access level detected. 2FA not required."
+    else
+        echo "Unknown user role. Please contact administrator."
+        exit 1
+    fi
+
+    # Set session start time and start session loop
+    SESSION_START_TIME=$(date +%s)
+    while true; do
+        check_session_timeout
+
+        # Insert main application logic here
+        echo "Access granted! Main application logic goes here."
+
+        # Wait for user input
+        read -p "Press enter to continue, or type 'logout' to end session: " input
+        if [[ $input == "logout" ]]; then
+            logout_user
+            log_authentication_attempt
+            break
+        fi
+    done
+}
+
+# Start the script
+main
